684eab071038b6cd98a242649bff43a6847d42b8
Todor Todorov <spindisamas@gmail.com>
2020-04-24 03:55:03 +0300

    Adding Homework 5

---
+++ Homework 5 - Healthbar/Assets/Scripts/Health.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using UnityEngine;
+using static UnityEngine.Mathf;
+
+public class Health : MonoBehaviour {
+
+	public event Action OnTakeDamage;
+
+	[SerializeField]
+	private int health = 100;
+
+	private Animator animator;
+	public GameObject cross;
+
+	void Start() {
+		animator = GetComponent<Animator>();
+	}
+
+	public void SpawnCross() {
+		Vector2 spawnPosition = new Vector2 {
+			x = transform.position.x,
+			y = -0.1f
+		};
+		Instantiate(cross, spawnPosition, Quaternion.identity);
+	}
+
+	public void Die() {
+		Destroy(gameObject);
+	}
+
+	public void TakeDamage() {
+		int damage = 10;
+		health = Max(health - damage, 0);
+		animator.SetInteger("Health", health);
+		animator.SetTrigger("TookDamage");
+
+		OnTakeDamage?.Invoke();
+	}
+
+	private void OnTriggerEnter2D(Collider2D collision) {
+		if (collision.transform.parent != transform
+			&& collision.gameObject.CompareTag("Hitbox")) {
+
+			TakeDamage();
+		}
+	}
+}
---
+++ Homework 5 - Healthbar/Assets/Scripts/UI_HealthManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_HealthManager : MonoBehaviour
+{
+    RectTransform[] healthBars;
+    Health health;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        HorizontalLayoutGroup group = GetComponentInChildren<HorizontalLayoutGroup>();
+        healthBars = group.GetComponentsInChildren<RectTransform>();
+
+        health = GetComponentInParent<Health>();
+    
+        health.OnTakeDamage += Health_OnTakeDamage;
+    }
+
+    private void Health_OnTakeDamage()
+    {
+        for (int i = healthBars.Length - 1; i >= 0; --i)
+        {
+            if (healthBars[i].gameObject.activeInHierarchy)
+            {
+                healthBars[i].gameObject.SetActive(false);
+                break;
+            }
+        }
+    }
+
+    private void OnDestroy()
+    {
+        health.OnTakeDamage -= Health_OnTakeDamage;
+    }
+}
